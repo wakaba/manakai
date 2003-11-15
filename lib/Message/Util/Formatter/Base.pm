fc38451e07b7998b91d2b9579350f53705408d9f
Wakaba <w@suika.fam.cx>
2003-11-15 12:30:42 +0000

    New modules

---
+++ lib/Message/Util/Formatter/Base.pm
@@ -0,0 +1,186 @@
+
+=head1 NAME
+
+Message::Util::Formatter::Base --- Manakai : Format text replacer
+
+=head1 DESCRIPTION
+
+This module is a base class to implement specific application
+of "formatting".
+
+This module is part of manakai.
+
+=cut
+
+package Message::Util::Formatter::Base;
+use strict;
+our $VERSION = do{my @r=(q$Revision: 1.1 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
+
+our %DefaultRules = (
+  -bare_text => {
+                 
+  },
+  -undef     => {
+             
+  },
+  -default   => {
+    pre => sub { },
+    post => sub { },
+    attr => sub { },                 
+  },
+  -entire    => {
+    
+  },
+);
+
+sub new ($;%) {
+  my ($class, %opt) = @_;
+  my $self = bless \%opt, $class;
+  if (ref $self->{rule}) {
+    if (ref $self->{rule} eq 'HASH') {
+      my $rules = $self->{rule};
+      $self->{rule} = sub { $rules->{$_[1]} };
+    }
+  } else {
+    $self->{rule} = sub { $DefaultRules{$_[1]} };
+  }
+  $self;
+}
+
+{
+our $__QuoteBlockContent;
+$__QuoteBlockContent = qr/[^{}]*(?:[^{}]|{(??{$__QuoteBlockContent})})*/;
+my $WordM = qr(
+                          ([\w-]+)                    ## Bare
+                       | {($__QuoteBlockContent)}     ## {Quoted}
+                       | "([^"\\]*(?>[^"\\]+|\\.)*)"  ## "Quoted"
+)x;
+
+sub replace ($$;%) {
+  my ($self, $format, %opt) = @_;
+  my $defrule = $self->{rule}->($self, '-default');
+  my $textrule = $self->{rule}->($self, '-bare_text');
+  my $undefrule = $self->{rule}->($self, '-undef');
+  my $entirerule = $self->{rule}->($self, '-entire');
+  my $result;
+  local $opt{param}->{-result} = \$result;
+  ($entirerule->{pre}||$defrule->{pre})->($self, '-entire',
+                                          $opt{param}, $opt{param});
+  pos ($format) = 0;
+  while (pos ($format) < length ($format)) {
+    if ($format =~ /\G%([\w-]+)\s*/gc) { # ":" is reserved for QName
+      my $name = $1;
+      $name =~ tr/-/_/;
+      my $rule = $self->{rule}->($self, $name) || $undefrule;
+      my $result;
+      my %attr = (-result => \$result);
+      ($rule->{pre}||$defrule->{pre})->($self, $name, \%attr, $opt{param});
+      $format =~ /\G\s+/gc;
+      
+      if ($format =~ /\G\(\s*/gc) {
+        while (1) {
+          if ($format =~ /\G$WordM\s*/gc) {
+            my $attr_name = $+;
+            $attr_name =~ s/\\(.)/$1/gs if defined $3; # "quoted"
+            $attr_name =~ tr/-/_/;
+            my $nflag;
+            $nflag = $1 if $format =~ /\G(\w+)\s*/gc;
+            if ($format =~ /\G=>\s*$WordM\s*/gc) {
+              my $attr_val = $+;
+              $attr_val =~ s/\\(.)/$1/gs if defined $3; # "quoted"
+              my $vflag;
+              $vflag = $1 if $format =~ /\G(\w+)\s*/gc;
+              ($rule->{attr}||$defrule->{attr})->($self, $name,
+                                                  \%attr, $opt{param},
+                                                  $attr_name => $attr_val,
+                                                  -name_flag => $nflag,
+                                                  -value_flag => $vflag);
+            } else {
+              ($rule->{attr}||$defrule->{attr})->($self, $name,
+                                                  \%attr, $opt{param},
+                                                  -boolean => $attr_name,
+                                                  -name_flag => $nflag);
+            }
+          } # An attribute specification
+          if ($format =~ /\G,\s*/gc) {
+            next;
+          } elsif ($format =~ /\G\)\s*/gc) {
+            last;
+          } else {
+            throw Message::Util::Formatter::Base::error
+              type => 'ATTR_SEPARATOR_NOT_FOUND',
+              -context_before => (pos ($format) > 10 ?
+                                  substr ($format, pos ($format) - 10, 10):
+                                  substr ($format, 0, pos ($format))),
+              -context_after => substr ($format, pos ($format), 10);
+          }
+        } # Attributes
+      } # Attribute specification list
+      if ($format =~ /\G;/gc) {
+        ($rule->{post}||$defrule->{post})->($self, $name,
+                                            \%attr,
+                                            $opt{param});
+      } else {
+        throw Message::Util::Formatter::Base::error
+          type => 'SEMICOLON_NOT_FOUND',
+          -context_before => (pos ($format) > 10 ?
+                              substr ($format, pos ($format) - 10, 10):
+                              substr ($format, 0, pos ($format))),
+          -context_after => substr ($format, pos ($format), 10);
+      }
+      ($entirerule->{attr}||$defrule->{attr})->($self, '-entire',
+                                                $opt{param}, $opt{param},
+                                                $name => $result);
+    } elsif ($format =~ /\G[^%]+(?:%[^\w-]|[^%]+)*/gc) {
+      my $result;
+      my %attr = (-result => \$result);
+      ($textrule->{pre}||$defrule->{pre})->($self, '-bare_text',
+                                            \%attr, $opt{param});
+      ($textrule->{attr}||$defrule->{attr})->($self, '-bare_text',
+                                              \%attr, $opt{param},
+                       -bare_text => substr ($format, $-[0], $+[0]-$-[0]));
+      ($textrule->{post}||$defrule->{post})->($self, '-bare_text',
+                                              \%attr, $opt{param});
+      ($entirerule->{attr}||$defrule->{attr})->($self, '-entire',
+                                                $opt{param}, $opt{param},
+                                                -bare_text => $result);
+    }
+  }
+  ($entirerule->{post}||$defrule->{post})->($self, '-entire',
+                                            $opt{param}, $opt{param});
+  $result;
+}
+}
+
+sub call ($$;@) {
+  my ($self, $name, $function) = (@_[0,1,2]);
+  ( ($self->{rule}->($self, $name) or $self->{rule}->($self, '-undef') )
+    ->{$function}
+  or $self->{rule}->($self, '-default')->{$function})
+  ->($self, $name, @_[3,$#_]);
+}
+
+package Message::Util::Formatter::error;
+require Message::Util::Error;
+our @ISA = 'Message::Util::Error';
+package Message::Util::Formatter::Base::error;
+our @ISA = 'Message::Util::Formatter::error';
+sub ___errors () {+{
+  ATTR_SEPARATOR_NOT_FOUND => {
+    description => q[Separator ("," or ")") expected at "%t(name=>context-before);"**here**"%t(name=>context-after);"],  
+  },
+  SEMICOLON_NOT_FOUND => {
+    description => q(Semicolon (";") expected at "%t(name=>context-before);"**here**"%t(name=>context-after);"),
+  },
+}}
+
+=head1 LICENSE
+
+Copyright 2003 Wakaba <w@suika.fam.cx>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1; # $Date: 2003/11/15 12:30:42 $
---
+++ lib/Message/Util/Formatter/Text.pm
@@ -0,0 +1,99 @@
+
+=head1 NAME
+
+Message::Util::Formatter::Text --- Manakai : Plain-text formatter
+
+=head1 DESCRIPTION
+
+This module is an application of Message::Util::Formatter mechanism,
+used to generate formatted plain text (fragment) with given formatting
+rule text and formatting functions.
+
+This module is part of manakai.
+
+=cut
+
+package Message::Util::Formatter::Text;
+use strict;
+our $VERSION = do{my @r=(q$Revision: 1.1 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
+require Message::Util::Formatter::Base;
+our @ISA = 'Message::Util::Formatter::Base';
+
+sub rule_def () {+{
+    -bare_text => {
+      post => sub {
+        my ($self, $name, $p, $o, $key => $val) = @_;
+        ${$p->{-result}} .= $p->{-bare_text};
+      },
+    },
+    -undef     => {
+      post => sub {
+        my ($self, $name, $p, $o) = @_;
+        ${$p->{-result}} = qq([undef: $name]);
+      },             
+    },
+    -default   => {
+      pre => sub { 
+        my ($self, $name, $p, $o, %opt) = @_;
+        ${$p->{-result}} = '';
+        $self->call ($name, 'before', $p, $o, %opt);
+      },
+      post => sub { 
+        my ($self, $name, $p, $o, %opt) = @_;
+        $self->call ($name, 'after', $p, $o, %opt);
+        if (length ${$p->{-result}} and 
+          (defined $p->{prefix} or defined $p->{suffix})) {
+          ${$p->{-result}} = $p->{prefix} . ${$p->{-result}} . $p->{suffix};
+        }
+      },
+      attr => sub {
+        my ($self, $name, $p, $o, $key, $val, %opt) = @_;
+        if ($opt{-value_flag} and index ($opt{-value_flag}, 'p') > -1) {
+          $val = $self->replace ($val, param => $o);
+        }
+        $p->{$key} = $val;
+      },                 
+      before => sub {
+      },
+      after => sub {
+      },
+    },
+    -entire    => {
+      pre => sub {
+        my ($self, $name, $p, $o) = @_;
+        ${$p->{-result}} = '';
+      },
+      attr => sub {
+        my ($self, $name, $p, $o, $key => $val) = @_;
+        ${$p->{-result}} .= $val;
+      },
+    },
+    percent => {
+      after => sub {
+        my ($self, $name, $p, $o) = @_;
+        ${$p->{-result}} = '%';
+      },
+    },
+}}
+
+sub new ($;%) {
+  my ($class, %opt) = @_;
+  my $rule = $opt{rule};
+  $opt{rule} = sub {
+    $_[0]->rule_def->{$_[1]}
+      or
+    __PACKAGE__->rule_def->{$_[1]}
+  };
+  $class->SUPER::new (%opt);
+}
+
+=head1 LICENSE
+
+Copyright 2003 Wakaba <w@suika.fam.cx>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1; # $Date: 2003/11/15 12:30:42 $
