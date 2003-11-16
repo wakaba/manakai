183aa5afa18b6979adb8c912ebed622dcf258744
Wakaba <w@suika.fam.cx>
2003-11-16 11:44:24 +0000

    New module

---
+++ lib/Message/Util/Formatter/Node.pm
@@ -0,0 +1,122 @@
+
+=head1 NAME
+
+Message::Util::Formatter::Node --- Manakai : Generating Node tree with formatting rule
+
+=head1 DESCRIPTION
+
+This module is an application of Message::Util::Formatter mechanism,
+used to generate a node tree (fragment) with given formatting
+rule text and formatting functions.
+
+Although this module is intended to be used in combination with 
+Message::Markup::XML::Node, it can be useful as a base class for
+other node module interface.
+
+This module is part of manakai.
+
+=cut
+
+package Message::Util::Formatter::Node;
+use strict;
+our $VERSION = do{my @r=(q$Revision: 1.1 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
+require Message::Util::Formatter::Base;
+our @ISA = 'Message::Util::Formatter::Base';
+
+sub rule_def () {+{
+    -attr_bare_text => {
+      main => sub {
+        my ($self, $name, $p, $o, %opt) = @_;
+        return $opt{-parent}->append_text ($opt{-attr_bare_text});
+      },
+    },
+    -bare_text => {
+      main => sub {
+        my ($self, $name, $p, $o, %opt) = @_;
+        return $p->{-parent}->append_text ($p->{-bare_text});
+      },
+    },
+    -undef     => {
+      main => sub {
+        my ($self, $name, $p, $o) = @_;
+        $p->{-parent}->append_text ("[undef: $name]");
+      },             
+    },
+    -default   => {
+      pre => sub { 
+        my ($self, $name, $p, $o, %opt) = @_;
+        local $p->{-parent} = $o->{-result};
+      },
+      post => sub { 
+        my ($self, $name, $p, $o, %opt) = @_;
+        local $p->{-parent} = $o->{-result};
+        $self->call ($name, 'main', $p, $o, %opt);
+      },
+      attr => sub {
+        my ($self, $name, $p, $o, $key, $val, %opt) = @_;
+        if ($opt{-value_flag} and index ($opt{-value_flag}, 'p') > -1) {
+          $p->{-parse_flag}->{$key} = 1;
+        }
+        $p->{$key} = $val;
+      },                 
+      main => sub {
+      },
+    },
+    -entire    => {
+      pre => sub {
+        my ($self, $name, $p, $o, %opt) = @_;
+        if (ref $opt{option}->{-parent}) {
+          $o->{-result} = $opt{option}->{-parent};
+        } else {
+          $o->{-result} = $opt{option}->{-class}->new
+            (type => '#fragment');
+        }
+      },
+      post => sub {
+      },
+      attr => sub {
+      },
+    },
+    percent => {
+      main => sub {
+        my ($self, $name, $p, $o) = @_;
+        $p->{-parent}->append_text ('%');
+      },
+    },
+}}
+
+sub new ($;%) {
+  my ($class, %opt) = @_;
+  my $rule = $opt{rule};
+  $opt{rule} = sub {
+    $_[0]->rule_def->{$_[1]}
+      or
+    __PACKAGE__->rule_def->{$_[1]}
+  };
+  $class->SUPER::new (%opt);
+}
+
+sub parse_attr ($$$$;%) {
+  my ($self, $p, $name, $o, %opt) = @_;
+  if ($p->{-parse_flag}->{$name}) {
+    $p->{$name} = $self->replace ($p->{$name}, param => $o, 
+                                  -parent => $opt{-parent},
+                                  %{$opt{option}});
+  } elsif ($opt{-non_parsed_to_node}) {
+    $self->call (-attr_bare_text => 'main', $p, $o,
+                 -attr_bare_text => $p->{$name},
+                 -parent => $opt{-parent},
+                 %{$opt{option}});
+  }
+}
+
+=head1 LICENSE
+
+Copyright 2003 Wakaba <w@suika.fam.cx>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1; # $Date: 2003/11/16 11:44:24 $
