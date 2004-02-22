26f71a79268160992fb2350d28175ffc5739fbc6
Wakaba <w@suika.fam.cx>
2004-02-22 01:46:02 +0000

    EMPTY_PFX implemented

--- lib/Message/Markup/XML/QName.pm
+++ lib/Message/Markup/XML/QName.pm
@@ -1,14 +1,12 @@
 
 =head1 NAME
 
-Message::Markup::XML::XPath --- manakai XML : XML Path Language (XPath) support
+Message::Markup::XML::QName - manakai: QName support
 
 =head1 DESCRIPTION
 
-This module implements abstracted XPath object and its
-serialization to the expression.
-
-To parse XPath expression, use Message::Markup::XML::XPath::Parser.
+C<Message::Markup::XML::QName> module implements QName related functions,
+such as QName to expanded name convertion.
 
 This module is part of manakai XML.
 
@@ -16,13 +14,14 @@ This module is part of manakai XML.
 
 package Message::Markup::XML::QName;
 use strict;
-our $VERSION = do{my @r=(q$Revision: 1.12 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
+our $VERSION = do{my @r=(q$Revision: 1.13 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
 use Char::Class::XML qw!InXML_NCNameStartChar InXMLNCNameChar!;
 use Exporter;
 our @ISA = qw/Exporter/;
 require Carp;
 
-our @EXPORT_OK = qw/DEFAULT_PFX NULL_URI UNDEF_URI NS_xml_URI NS_xmlns_URI
+our @EXPORT_OK = qw/DEFAULT_PFX ZERO_PFX EMPTY_PFX
+                    NULL_URI UNDEF_URI NS_xml_URI NS_xmlns_URI
                     ZERO_URI EMPTY_URI/;
 our %EXPORT_TAGS = (
   prefix => [qw/DEFAULT_PFX/],
@@ -31,6 +30,8 @@ our %EXPORT_TAGS = (
 );
 
 sub DEFAULT_PFX () { q:#default: }
+sub EMPTY_PFX   () { q:#empty: }
+sub ZERO_PFX    () { q:#zero: }
 sub NULL_URI    () { q<http://suika.fam.cx/~wakaba/-temp/2003/09/27/null> }
 sub UNDEF_URI   () { q<http://suika.fam.cx/~wakaba/-temp/2003/09/27/undef> }
 sub ZERO_URI    () { q<http://suika.fam.cx/~wakaba/-temp/2003/12/05/zero> }
@@ -38,6 +39,77 @@ sub EMPTY_URI   () { q<http://suika.fam.cx/~wakaba/-temp/2003/12/05/empty> }
 sub NS_xml_URI  () { q<http://www.w3.org/XML/1998/namespace> }
 sub NS_xmlns_URI() { q<http://www.w3.org/2000/xmlns/> }
 #sub NS_INVALID_URI(){q<http://suika.fam.cx/~wakaba/-temp/2003/05/17/unknown-namespace#> }
+
+=head1 CONSTANTS
+
+C<Message::Markup::XML::QName> provides some constant functions
+represent special URI references or namespace prefix.
+These functions can be imported by C<use>'ing as:
+
+  use Message::Markup::XML::QName qw(DEFAULT_PFX NULL_URI);
+
+=over 4
+
+=item DEFAULT_PFX = "#default"
+
+Default namespace prefix, i.e. no prefix.
+
+Note that some applications (eg. XSLT) interprets C<#default>
+as no prefix, so that this constant function can be specified instead
+of such literal value.
+
+=item EMPTY_URI
+
+Empty URI reference.  Manakai XML modules use this constant function
+internally, since Perl considers empty (zero-length) string as false.
+
+Empty URI reference is not allowed by XML Namespace specification
+but can be used with other specification, suchs as DOM 2.
+
+=item NS_xml_URI
+
+Namespace name URI for C<xml>.  Namespace prefix C<xml> and namespace name
+corresponding to it is reserved by XML and XML Namespace specifications
+and took special treatement.
+
+=item NS_xmlns_URI
+
+Namespace name URI for C<xmlns>.  Namespace prefix C<xmlns> and 
+namespace name corresponding to it is reserved by XML and XML Namespace
+specifications and took special treatement.
+
+=item NULL_URI
+
+Null namespace name.  In XML, namespace declaration C<xmlns="">
+makes namespace name binded to default namespace (ie. C<DEFAULT_PFX> namespace)
+null.  Manakai XML modules use constant function C<NULL_URI> for
+null namespace.
+
+=item UNDEF_URI
+
+Namespace name is undefined.  In other word, namespace prefix is 
+binded to no namespace name.  In XML 1.1, this is represented by
+namespace declaration C<xmlns:I<prefix>="">.
+
+=item ZERO_URI
+
+Namespace name that is exact C<0>.  Manakai XML modules use this constant
+function for internal procedures, since C<0> is considered as false in Perl.
+
+Note that using relative URI reference as namespace name is deprecated.
+
+=back
+
+Don't try to use these constant functions to place left hand of C<< => >>
+operator, which make Perl interprets left hand bare value as a string.
+
+  x  something (DEFAULT_PFX => NULL_URI);
+  o  something (DEFAULT_PFX () => NULL_URI);
+  o  something ((DEFAULT_PFX) => NULL_URI);
+  o  something (+DEFAULT_PFX => NULL_URI);
+  o  something (DEFAUT_PFX, NULL_URI);
+
+=cut
 
 our %Namespace_URI_to_prefix 
   = (
@@ -64,6 +136,12 @@ our %Namespace_URI_to_prefix
 	'urn:x-suika-fam-cx:markup:ietf:rfc:2629:'	=> [DEFAULT_PFX, qw/rfc rfc2629/],
 );
 
+=head1 FUNCTIONS
+
+@@
+
+=cut
+
 sub register_prefix_to_name ($$$;%) {
   my ($decls, $prefix => $name, %opt) = @_;
   if ($opt{check_prefix}) {
@@ -71,7 +149,7 @@ sub register_prefix_to_name ($$$;%) {
     return $chk unless $chk->{success};
     $prefix = $chk->{prefix};
   } else {
-    $prefix = DEFAULT_PFX if $prefix eq '';
+    $prefix = $opt{use_prefix_empty} ? EMPTY_PFX : DEFAULT_PFX if $prefix eq '';
   }
   return {success => 0, reason => 'NAME'} if $name eq UNDEF_URI;
   if ($opt{check_name}) {
@@ -131,6 +209,8 @@ sub __check_prefix ($$$) {
     return {success => 0, prefix => DEFAULT_PFX, reason => 'PREFIX_DEFAULT'}
       unless $opt->{use_prefix_default};
     $prefix = DEFAULT_PFX;
+  } elsif ($opt->{use_prefix_empty} and $prefix eq EMPTY_PFX) {
+    #
   } else {
     return {success => 0, prefix => $prefix, reason => 'PREFIX_XML'}
       if $opt->{check_prefix_xml} && ($prefix eq 'xml');
@@ -164,8 +244,9 @@ sub __check_name ($$$) {
         if $name =~ m{[^0-9A-Za-z_.!~*'();/?:\@&=+\$,%\[\]#-]};
     }
     if ($opt->{resolve_name_uri_relative}) {
-      $name = $decls->resolve_relative_uri ($name)
-        unless $name =~ /^[0-9A-Za-z.%+-]+:/;
+      require Message::Markup::XML::NodeTree,
+      $name = $decls->Message::Markup::XML::NodeTree::resolve_relative_uri 
+                ($name) unless $name =~ /^[0-9A-Za-z.%+-]+:/;
     } elsif ($opt->{check_name_uri_relative}) {
       return {success => 0, reason => 'NAME_URI_RELATIVE'}
         unless $name =~ /^[0-9A-Za-z.%+-]+:/;
@@ -346,8 +427,12 @@ sub split_qname ($;%) {
   my ($pfx, $ln) = split /\Q$opt{qname_separator}/, $qname, 2;
   
   if ($opt{check_qname}) {
-    if ((substr ($qname, 0,  1) eq $opt{qname_separator})
-     || (substr ($qname, 0, -1) eq $opt{qname_separator})) {
+    if ($opt{use_prefix_empty}) {
+      return {success => 0, reason => 'QNAME__INVALID_COLON'}
+        if substr ($qname, -1) eq $opt{qname_separator};
+    } elsif (
+        (substr ($qname, 0,  1) eq $opt{qname_separator})
+     || (substr ($qname, -1) eq $opt{qname_separator})) {
       return {success => 0, reason => 'QNAME__INVALID_COLON'};
     }
   }
@@ -355,13 +440,19 @@ sub split_qname ($;%) {
   if (defined $ln) {
     if ($opt{check_qname} || $opt{check_prefix}) {
       return {success => 0, reason => 'PREFIX__INVALID'}
-        unless $pfx =~ /^\p{InXML_NCNameStartChar}\p{InXMLNCNameChar}*$/;
+        unless $pfx =~ /^\p{InXML_NCNameStartChar}\p{InXMLNCNameChar}*$/ or
+              ($opt{use_prefix_empty} and $pfx eq '');
     }
     if ($opt{check_qname} || $opt{check_local_name}) {
       return {success => 1, prefix => $pfx, local_name_star => 1}
         if $ln eq '*';
       return {success => 0, reason => 'LOCAL_NAME__INVALID'}
         unless $ln =~ /^\p{InXML_NCNameStartChar}\p{InXMLNCNameChar}*$/;
+    }
+    if ($pfx eq '') {
+      $pfx = EMPTY_PFX;
+    } elsif ($pfx eq '0') {
+      $pfx = ZERO_PFX;
     }
     return {success => 1, prefix => $pfx, local_name => $ln};
   } else {
@@ -380,7 +471,8 @@ sub join_qname ($$;%) {
   $pfx = DEFAULT_PFX unless defined $pfx;
   $opt{qname_separator} ||= ':';
   if ($opt{check_qname} || $opt{check_prefix}) {
-    if ($pfx ne DEFAULT_PFX) {
+    if ($pfx ne DEFAULT_PFX and
+        not ($pfx eq EMPTY_PFX and $opt{use_prefix_empty})) {
       return {success => 0, reason => 'PREFIX__INVALID'}
         unless $pfx =~ /^\p{InXML_NCNameStartChar}\p{InXMLNCNameChar}*$/;
     }
@@ -393,6 +485,8 @@ sub join_qname ($$;%) {
       unless $ln =~ /^\p{InXML_NCNameStartChar}\p{InXMLNCNameChar}*$/;
   }
   return {success => 1, qname => ($pfx eq DEFAULT_PFX ? $ln :
+                                  $pfx eq EMPTY_PFX ? $opt{qname_separator}.$ln:
+                                  $pfx eq ZERO_PFX?'0'.$opt{qname_separator}.$ln:
                                   $pfx.$opt{qname_separator}.$ln)};
 }
 
@@ -416,11 +510,11 @@ sub split_expanded_iri ($) {
 
 =head1 LICENSE
 
-Copyright 2003 Wakaba <w@suika.fam.cx>
+Copyright 2003-2004 Wakaba <w@suika.fam.cx>
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
 =cut
 
-1; # $Date: 2004/02/14 11:07:50 $
+1; # $Date: 2004/02/22 01:46:02 $
