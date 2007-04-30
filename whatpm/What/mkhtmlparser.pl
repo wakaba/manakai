d9dc0edc4c7ba14890ab51008541cdf3f062149b
Wakaba <w@suika.fam.cx>
2007-04-30 07:41:50 +0000

    ++ whatpm/What/ChangeLog	30 Apr 2007 07:41:35 -0000 2007-04-30  Wakaba  <wakaba@suika.fam.cx>
    	* HTML.pm.src: The tree construction stage is implemented.
    
    	* mkhtmlparser.pl: New macros are added.

--- whatpm/What/mkhtmlparser.pl
+++ whatpm/What/mkhtmlparser.pl
@@ -24,5 +24,55 @@ while (<>) {
   s{!!!back-next-input-character\b}{q{unshift @{$self->{char}}, }}ge;
   s{!!!parse-error;}{q{$self->{parse_error}->();}}ge;
   s{!!!parse-error\b}{q{$self->{parse_error}->}}ge;
+  s{!!!insert-element-t\s*\(([^(),]+),([^(),]+)\)\s*;}{qq{
+    {
+      my \$el;
+      !!!create-element (\$el, $1, $2);
+      \$insert->(\$el);
+      push \@\$open_elements, [\$el, $1];
+    }
+  }}ge;
+  s{!!!insert-element-t\s*\(([^(),]+)\)\s*;}{qq{
+    {
+      my \$el;
+      !!!create-element (\$el, $1);
+      \$insert->(\$el);
+      push \@\$open_elements, [\$el, $1];
+    }
+  }}ge;
+  s{!!!insert-element\s*\(([^(),]+),([^(),]+)\)\s*;}{qq{
+    {
+      my \$el;
+      !!!create-element (\$el, $1, $2);
+      \$open_elements->[-1]->[0]->append_child (\$el);
+      push \@\$open_elements, [\$el, $1];
+    }
+  }}ge;
+  s{!!!insert-element\s*\(([^(),]+)\)\s*;}{qq{
+    {
+      my \$el;
+      !!!create-element (\$el, $1);
+      \$open_elements->[-1]->[0]->append_child (\$el);
+      push \@\$open_elements, [\$el, $1];
+    }
+  }}ge;
+  s{!!!create-element\s*\(([^(),]+),([^(),]+)(?:,([^(),]+))?\)\s*;}{
+    my $r = qq{
+      $1 = \$self->{document}->create_element_ns
+        (q<http://www.w3.org/1999/xhtml>, [undef, $2]);
+    };
+    if (defined $3) {
+      $r .= qq{
+        for my \$attr_name (keys %{$3}) {
+          $1->set_attribute_ns (undef, [undef, \$attr_name],
+                                $3 ->{\$attr_name}->{value});
+        }
+      };
+    }
+    $r;
+  }ge; # MUST
+  s{!!!next-token;}{q{$token = $self->_get_next_token;}}ge;
+  s{!!!back-token;}{q{unshift @{$self->{token}}, $token;}}ge;
+  s{!!!back-token\s*\(}{q{unshift @{$self->{token}}, (}}ge;
   print;
 }
