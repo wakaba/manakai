3d52487438c0abbb9a26413ee628fe7fd0ed8358
Wakaba <w@suika.fam.cx>
2007-09-23 07:57:00 +0000

    ++ manakai/lib/Message/CGI/ChangeLog	23 Sep 2007 07:56:57 -0000 2007-09-23  Wakaba  <wakaba@suika.fam.cx>
    	* Makefile (all): |Carp.html| is added.
    
    ++ manakai/lib/Message/DOM/ChangeLog	23 Sep 2007 07:55:19 -0000
    2007-09-23  Wakaba  <wakaba@suika.fam.cx>
    
    	* SelectorsAPI.pm: New Perl module.

---
+++ lib/Message/CGI/Carp.pm
@@ -0,0 +1,143 @@
+
+=head1 NAME
+
+SuikaWiki::Output::CGICarp --- SuikaWiki : CGI::Carp modification to
+return 500 status code
+
+=head1 DESCRIPTION
+
+At the time of writing, latest version of CGI::Carp (1.27)
+does not output error dying message with 500 (Internal Server Error)
+HTTP status code unless it is working on mod_perl.
+
+This module overrides some of CGI::Carp functions to return 500 status
+code.  Users of this module should take attention whether later revision
+of CGI::Carp provides improved version of CGI outputing methods.
+
+This module is part of SuikaWiki.
+
+=cut
+
+package SuikaWiki::Output::CGICarp;
+require CGI::Carp; # 1.27
+use strict;
+our $VERSION = do{my @r=(q$Revision: 1.1 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
+our $CUSTOM_REASON_TEXT = 'Internal CGI Script Error';
+our $CUSTOM_STATUS_CODE = 500;
+
+=head1 OPTIONS
+
+=over 4
+
+=item $SuikaWiki::Output::CGICarp::CUSTOM_REASON_TEXT (Default : Internal CGI Script Error)
+
+Short description of error status.  This string should contains only
+printable ASCII characters (including SPACE) for interoperability
+and line break characters (CR and LF) MUST NOT be used.
+
+=item $SuikaWiki::Output::CGICarp::CUSTOM_STATUS_CODE (Default : 500)
+
+Three digit status code defined by HTTP specifications.
+
+=back
+
+=cut
+
+package CGI::Carp;
+our $CUSTOM_MSG;
+sub fatalsToBrowser {
+  my($msg) = @_;
+  $msg=~s/&/&amp;/g;
+  $msg=~s/>/&gt;/g;
+  $msg=~s/</&lt;/g;
+  $msg=~s/\"/&quot;/g;
+  my($wm) = $main::ENV{SERVER_ADMIN} ? 
+    qq[the webmaster &lt;<a href="mailto:$main::ENV{SERVER_ADMIN}">$main::ENV{SERVER_ADMIN}</a>&gt;] :
+      "this site's webmaster";
+  my ($outer_message) = <<END;
+For help, please send mail to $wm, giving this error message 
+and the time and date of the error.
+END
+  ;
+  my $mod_perl = exists $main::ENV{MOD_PERL};
+
+  warningsToBrowser(1);    # emit warnings before dying
+
+  if ($CUSTOM_MSG) {
+    if (ref($CUSTOM_MSG) eq 'CODE') {
+      my $bytes_written = eval{tell STDOUT};
+      unless (defined $bytes_written && $bytes_written > 0) {
+        print STDOUT "Status: @{[$SuikaWiki::Output::CGICarp::CUSTOM_STATUS_CODE+0]} $SuikaWiki::Output::CGICarp::CUSTOM_REASON_TEXT\n";
+        print STDOUT "Content-type: text/html; charset=iso-8859-1\n\n";
+      }
+      &$CUSTOM_MSG($msg); # nicer to perl 5.003 users
+      return;
+    } else {
+      $outer_message = $CUSTOM_MSG;
+    }
+  }
+
+  my $mess = <<END;
+<h1>Software error:</h1>
+<pre>$msg</pre>
+<p>
+$outer_message
+</p>
+END
+  ;
+
+  if ($mod_perl) {
+    require mod_perl;
+    if ($mod_perl::VERSION >= 1.99) {
+      $mod_perl = 2;
+      require Apache::RequestRec;
+      require Apache::RequestIO;
+      require Apache::RequestUtil;
+      require APR::Pool;
+      require ModPerl::Util;
+      require Apache::Response;
+    }
+    my $r = Apache->request;
+    # If bytes have already been sent, then
+    # we print the message out directly.
+    # Otherwise we make a custom error
+    # handler to produce the doc for us.
+    if ($r->bytes_sent) {
+      $r->print($mess);
+      $mod_perl == 2 ? ModPerl::Util::exit(0) : $r->exit;
+    } else {
+      # MSIE won't display a custom 500 response unless it is >512 bytes!
+      if ($main::ENV{HTTP_USER_AGENT} =~ /MSIE/) {
+        $mess = "<!-- " . (' ' x 513) . " -->\n$mess";
+      }
+      $r->custom_response($SuikaWiki::Output::CGICarp::CUSTOM_STATUS_CODE+0,$mess);
+    }
+  } else {
+    my $bytes_written = eval{tell STDOUT};
+    if (defined $bytes_written && $bytes_written > 0) {
+      print STDOUT $mess;
+    } else {
+      print STDOUT "Status: @{[$SuikaWiki::Output::CGICarp::CUSTOM_STATUS_CODE+0]} $SuikaWiki::Output::CGICarp::CUSTOM_REASON_TEXT\n";
+      print STDOUT "Content-type: text/html; charset=iso-8859-1\n\n";
+      print STDOUT $mess;
+    }
+  }
+}
+
+=head1 EXAMPLE
+
+  use CGI::Carp qw/fatalsToBrowser/;
+  require SuikaWiki::Output::CGICarp;
+  
+  die 'Something wrong';
+
+=head1 LICENSE
+
+Copyright 2003-2004 Wakaba <w@suika.fam.cx>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1; # $Date: 2007/09/23 07:57:00 $
---
+++ lib/Message/DOM/SelectorsAPI.pm
@@ -0,0 +1,136 @@
+package Message::DOM::SelectorsAPI;
+use strict;
+our $VERSION=do{my @r=(q$Revision: 1.1 $=~/\d+/g);sprintf "%d."."%02d" x $#r,@r};
+
+package Message::DOM::Document;
+
+use Whatpm::CSS::SelectorsParser qw(:match :combinator :selector);
+
+sub query_selectors_all ($$;$) {
+  local $Error::Depth = $Error::Depth + 1;
+
+  my $p = Whatpm::CSS::SelectorsParser->new;
+  $p->{pseudo_class}->{$_} = 1 for qw/
+  /;
+#    active checked disabled empty enabled first-child first-of-type
+#    focus hover indeterminate last-child last-of-type link only-child
+#    only-of-type root target visited
+#    lang nth-child nth-last-child nth-of-type nth-last-of-type not
+  $p->{pseudo_element}->{$_} = 1 for qw/
+  /;
+#    after before first-letter first-line
+  my $selectors = $p->parse_string (''.$_[1]);
+  my $r = [];
+
+  use Whatpm::CSS::SelectorsSerializer;
+  warn Whatpm::CSS::SelectorsSerializer->serialize_test ($selectors);
+
+  ## TODO: invalid selectors
+
+  my $is_html = ($_[0]->owner_document || $_[0])->manakai_is_html;
+  
+  my @node_cond = map {[$_, [@$selectors]]} @{$_[0]->child_nodes};
+  while (@node_cond) {
+    my $node_cond = shift @node_cond;
+    if ($node_cond->[0]->node_type == 1) { # ELEMENT_NODE
+      my @new_cond;
+      my $matched;
+      for my $selector (@{$node_cond->[1]}) {
+        my $sss_matched = 1;
+        for my $simple_selector (@{$selector->[1]}) {
+          if ($simple_selector->[0] == LOCAL_NAME_SELECTOR) {
+            if ($simple_selector->[1] eq
+                $node_cond->[0]->manakai_local_name) {
+              #
+            } elsif ($is_html) {
+              my $nsuri = $node_cond->[0]->namespace_uri;
+              if (defined $nsuri and
+                  $nsuri eq q<http://www.w3.org/1999/xhtml>) {
+                if (lc $simple_selector->[1] eq
+                    $node_cond->[0]->manakai_local_name) {
+                  ## TODO: What kind of case-insensitivility?
+                  ## TODO: Is this checking method OK?
+                  #
+                } else {
+                  $sss_matched = 0;
+                }
+              } else {
+                $sss_matched = 0;
+              }
+            } else {
+              $sss_matched = 0;
+            }
+          } else {
+            die "$0: $simple_selector->[0]: Unknown simple selector type";
+          }
+        }
+        
+        if ($sss_matched) {
+          if (@$selector == 2) {
+            push @$r, $node_cond->[0] unless $matched;
+            $matched = 1;
+          } else {
+            my $new_selector = [@$selector[2..$#$selector]];
+            if ($new_selector->[0] == DESCENDANT_COMBINATOR or
+                $new_selector->[0] == CHILD_COMBINATOR) {
+              push @new_cond, $new_selector;
+            } else { # ADJACENT_SIBLING_COMBINATOR | GENERAL_SIBLING_COMBINATOR
+              push @{$node_cond->[2]->[1] || []}, $new_selector;
+            }
+          }
+        }
+        if ($selector->[0] == DESCENDANT_COMBINATOR) {
+          push @new_cond, $selector;
+        } elsif ($selector->[0] == GENERAL_SIBLING_COMBINATOR) {
+          push @{$node_cond->[2]->[1] || []}, $selector;
+        }
+      }
+
+      if (@new_cond) {
+        my @children = grep { # ELEMENT_NODE or ENTITY_REFERENCE_NODE
+          $_->node_type == 1 or $_->node_type == 5
+        } @{$node_cond->[0]->child_nodes};
+        my $next_sibling_cond;
+        for (reverse @children) {
+          my $new_node_cond = [$_, [@new_cond], $next_sibling_cond];
+          unshift @node_cond, $new_node_cond;
+          $next_sibling_cond = $new_node_cond;
+        }
+      }
+    } elsif ($node_cond->[0]->node_type == 5) { # ENTITY_REFERENCE_NODE
+      my @new_cond = @{$node_cond->[1]};
+      my @new_cond2 = grep {
+        $_->[0] != ADJACENT_SIBLING_COMBINATOR and
+        $_->[0] != GENERAL_SIBLING_COMBINATOR
+      } @new_cond;
+      my @children = grep { # ELEMENT_NODE or ENTITY_REFERENCE_NODE
+        $_->node_type == 1 or $_->node_type == 5
+      } @{$node_cond->[0]->child_nodes};
+      my $next_sibling_cond;
+      for (reverse @children) {
+        my $new_node_cond = [$_, \@new_cond2, $next_sibling_cond];
+        unshift @node_cond, $new_node_cond;
+        $next_sibling_cond = $new_node_cond;
+      }
+      $next_sibling_cond->[1] = \@new_cond;
+    }
+  }
+  return $r;
+} # get_elements_by_selectors
+
+package Message::DOM::Element;
+
+package Message::DOM::DocumentSelector;
+package Message::DOM::ElementSelector;
+
+=head1 LICENSE
+
+Copyright 2007 Wakaba <w@suika.fam.cx>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1;
+## $Date: 2007/09/23 07:57:00 $
