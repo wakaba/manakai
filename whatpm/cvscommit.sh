7c4372fe107d26a0a3637635b22edd9cf633a31f
cvs2git <cvs@suika.fam.cx>
2007-09-29 09:02:55 +0000

    This commit was manufactured by cvs2svn to create tag 'manakai- release-0-4-0'.

--- whatpm/cvscommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-find -name ChangeLog | xargs cvs diff | grep "^\+" | sed -e "s/^\+//; s/^\+\+ .\//++ whatpm\//" > .cvslog.tmp
-cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
-rm .cvslog.tmp
-
-## $Date: 2007/04/24 13:35:21 $
-## License: Public Domain
