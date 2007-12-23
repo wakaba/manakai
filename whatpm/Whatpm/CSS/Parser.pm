4614958640b33bae7f9255abaa27e96ab1a757de
Wakaba <w@suika.fam.cx>
2007-12-23 08:16:26 +0000

    New

---
+++ whatpm/Whatpm/CSS/Parser.pm
@@ -0,0 +1,216 @@
+package Whatpm::CSS::Parser;
+use strict;
+use Whatpm::CSS::Tokenizer qw(:token);
+require Whatpm::CSS::SelectorsParser;
+
+sub new ($) {
+  my $self = bless {onerror => sub { }, must_level => 'm'}, shift;
+
+  return $self;
+} # new
+
+sub BEFORE_STATEMENT_STATE () { 0 }
+sub BEFORE_DECLARATION_STATE () { 1 }
+sub IGNORED_STATEMENT_STATE () { 2 }
+sub IGNORED_DECLARATION_STATE () { 3 }
+
+sub parse_char_string ($$) {
+  my $self = $_[0];
+
+  my $s = $_[1];
+  pos ($s) = 0;
+  
+  my $tt = Whatpm::CSS::Tokenizer->new;
+  $tt->{onerror} = $self->{onerror};
+  $tt->{get_char} = sub {
+    if (pos $s < length $s) {
+      return ord substr $s, pos ($s)++, 1;
+    } else {
+      return -1;
+    }
+  }; # $tt->{get_char}
+  $tt->init;
+
+  my $sp = Whatpm::CSS::SelectorsParser->new;
+  $sp->{onerror} = $self->{onerror};
+  $sp->{must_level} = $self->{must_level};
+
+  ## TODO:
+  #$sp->{lookup_namespace_uri} = ...;
+
+  ## TODO: Supported pseudo classes and elements...
+
+  require Message::DOM::CSSStyleSheet;
+  require Message::DOM::CSSRule;
+  require Message::DOM::CSSStyleDeclaration;
+
+  my $state = BEFORE_STATEMENT_STATE;
+  my $t = $tt->get_next_token;
+
+  my $open_rules = [[]];
+  my $current_rules = $open_rules->[-1];
+  my $current_decls;
+  my $closing_tokens = [];
+
+  S: {
+    if ($state == BEFORE_STATEMENT_STATE) {
+      $t = $tt->get_next_token
+          while $t->{type} == S_TOKEN or
+              $t->{type} == CDO_TOKEN or
+              $t->{type} == CDC_TOKEN;
+
+      if ($t->{type} == ATKEYWORD_TOKEN) {
+        ## TODO: supported...
+
+        $t = $tt->get_next_token;
+        $state = IGNORED_STATEMENT_STATE;
+        redo S;
+      } elsif (@$open_rules > 1 and $t->{type} == RBRACE_TOKEN) {
+        pop @$open_rules;
+        ## Stay in the state.
+        $t = $tt->get_next_token;
+        redo S;
+      } elsif ($t->{type} == EOF_TOKEN) {
+        if (@$open_rules > 1) {
+          $self->{onerror}->(type => 'syntax error:block not closed',
+                             level => $self->{must_level},
+                             token => $t);
+        }
+
+        last S;
+      } else {
+        ($t, my $selectors) = $sp->_parse_selectors_with_tokenizer
+            ($tt, LBRACE_TOKEN, $t);
+
+        $t = $tt->get_next_token
+            while $t->{type} != LBRACE_TOKEN and $t->{type} != EOF_TOKEN;
+
+        if ($t->{type} == LBRACE_TOKEN) {
+          $current_decls = Message::DOM::CSSStyleDeclaration->____new;
+          my $rs = Message::DOM::CSSStyleRule->____new
+              ($selectors, $current_decls);
+          push @{$current_rules}, $rs if defined $selectors;
+
+          $state = BEFORE_DECLARATION_STATE;
+          $t = $tt->get_next_token;
+          redo S;
+        } else {
+          $self->{onerror}->(type => 'syntax error:after selectors',
+                             level => $self->{must_level},
+                             token => $t);
+
+          ## Stay in the state.
+          $t = $tt->get_next_token;
+          redo S;
+        }
+      }
+    } elsif ($state == BEFORE_DECLARATION_STATE) {
+      ## NOTE: DELIM? in declaration will be removed:
+      ## <http://csswg.inkedblade.net/spec/css2.1?s=declaration%20delim#issue-2>.
+
+      $t = $tt->get_next_token while $t->{type} == S_TOKEN;
+      if ($t->{type} == IDENT_TOKEN) { # property
+        ## TODO: If supported, ...
+
+        $t = $tt->get_next_token;
+        #
+      } elsif ($t->{type} == RBRACE_TOKEN) {
+        $t = $tt->get_next_token;
+        $state = BEFORE_STATEMENT_STATE;
+        redo S;
+      } elsif ($t->{type} == EOF_TOKEN) {
+        $self->{onerror}->(type => 'syntax error:ruleset not closed',
+                           level => $self->{must_level},
+                           token => $t);
+        ## Reprocess.
+        $state = BEFORE_STATEMENT_STATE;
+        redo S;
+      }
+
+      #
+      $state = IGNORED_DECLARATION_STATE;
+      redo S;
+    } elsif ($state == IGNORED_STATEMENT_STATE or
+             $state == IGNORED_DECLARATION_STATE) {
+      if (@$closing_tokens) { ## Something is yet in opening state.
+        if ($t->{type} == EOF_TOKEN) {
+          @$closing_tokens = ();
+          ## Reprocess.
+          $state = $state == IGNORED_STATEMENT_STATE
+              ? BEFORE_STATEMENT_STATE : BEFORE_DECLARATION_STATE;
+          redo S;
+        } elsif ($t->{type} == $closing_tokens->[-1]) {
+          pop @$closing_tokens;
+          if (@$closing_tokens == 0 and
+              $t->{type} == RBRACE_TOKEN and
+              $state == IGNORED_STATEMENT_STATE) {
+            $t = $tt->get_next_token;
+            $state = BEFORE_STATEMENT_STATE;
+            redo S;
+          } else {
+            $t = $tt->get_next_token;
+            ## Stay in the state.
+            redo S;
+          }
+        } else {
+          #
+        }
+      } else {
+        if ($t->{type} == SEMICOLON_TOKEN) {
+          $t = $tt->get_next_token;
+          $state = $state == IGNORED_STATEMENT_STATE
+              ? BEFORE_STATEMENT_STATE : BEFORE_DECLARATION_STATE;
+          redo S;
+        } elsif ($state == IGNORED_DECLARATION_STATE and
+                 $t->{type} == RBRACE_TOKEN) {
+          $t = $tt->get_next_token;
+          $state = BEFORE_STATEMENT_STATE;
+          redo S;
+        } elsif ($t->{type} == EOF_TOKEN) {
+          ## Reprocess.
+          $state = $state == IGNORED_STATEMENT_STATE
+              ? BEFORE_STATEMENT_STATE : BEFORE_DECLARATION_STATE;
+          redo S;
+        } else {
+          #
+        }
+      }
+
+      while (not {
+        EOF_TOKEN, 1,
+        RBRACE_TOKEN, 1,
+        RBRACKET_TOKEN, 1,
+        RPAREN_TOKEN, 1,
+        SEMICOLON_TOKEN, 1,
+      }->{$t->{type}}) {
+        if ($t->{type} == LBRACE_TOKEN) {
+          push @$closing_tokens, RBRACE_TOKEN;
+        } elsif ($t->{type} == LBRACKET_TOKEN) {
+          push @$closing_tokens, RBRACKET_TOKEN;
+        } elsif ($t->{type} == LPAREN_TOKEN or $t->{type} == FUNCTION_TOKEN) {
+          push @$closing_tokens, RPAREN_TOKEN;
+        }
+
+        $t = $tt->get_next_token;
+      }
+
+      #
+      ## Stay in the state.
+      redo S;
+    } else {
+      die "$0: parse_char_string: Unknown state: $state";
+    }
+  } # S
+
+  my $ss = Message::DOM::CSSStyleSheet->____new
+      (css_rules => $open_rules->[0],
+       ## TODO: href
+       ## TODO: owner_node
+       ## TODO: media
+       type => 'text/css', ## TODO: OK?
+       _parser => $self);
+  return $ss;
+} # parse_char_string
+
+1;
+## $Date: 2007/12/23 08:16:26 $
