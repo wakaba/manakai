e9ed495e44492e64e81a1b7748cab23c8d2f8fec
Wakaba <w@suika.fam.cx>
2008-09-18 05:31:46 +0000

    ++ whatpm/t/ChangeLog	18 Sep 2008 05:31:37 -0000 2008-09-18  Wakaba  <wakaba@suika.fam.cx>
    	* ContentChecker.t: Test engine moved to another script.
    
    	* content-checker.pl: New script.

---
+++ whatpm/t/content-checker.pl
@@ -0,0 +1,140 @@
+use strict;
+
+## ISSUE: Currently we require manakai XML parser to test arbitrary XML tree.
+use lib qw[/home/wakaba/work/manakai2/lib];
+
+use Test;
+require Whatpm::ContentChecker;
+require Message::DOM::DOMImplementation;
+require Message::DOM::XMLParserTemp;
+require Whatpm::HTML;
+require Whatpm::NanoDOM;
+
+sub test_files (@) {
+  my @FILES = @_;
+
+  for my $file_name (@FILES) {
+    open my $file, '<', $file_name or die "$0: $file_name: $!";
+    print "# $file_name\n";
+    
+    my $test;
+    my $mode = 'data';
+    while (<$file>) {
+      s/\x0D\x0A/\x0A/;
+      if (/^#data$/) {
+        undef $test;
+        $test->{data} = '';
+        $mode = 'data';
+        $test->{parse_as} = 'xml';
+      } elsif (/^#data html$/) {
+        undef $test;
+        $test->{data} = '';
+        $mode = 'data';
+        $test->{parse_as} = 'html';
+      } elsif (/^#errors$/) {
+        $test->{errors} = [];
+        $mode = 'errors';
+        $test->{data} =~ s/\x0D?\x0A\z//;       
+      } elsif (defined $test->{errors} and /^$/) {
+        test ($test);
+        undef $test;
+      } else {
+        if ($mode eq 'data') {
+          $test->{$mode} .= $_;
+        } elsif ($mode eq 'errors') {
+          tr/\x0D\x0A//d;
+          push @{$test->{errors}}, $_;
+        }
+      }
+    }
+  } # @FILES
+} # test_files
+
+my $dom = Message::DOM::DOMImplementation->new;
+sub test ($) {
+  my $test = shift;
+
+  my $doc;
+  if ($test->{parse_as} eq 'xml') {
+    open my $fh, '<', \($test->{data});
+    $doc = Message::DOM::XMLParserTemp->parse_byte_stream
+      ($fh => $dom, sub { }, charset => 'utf-8');
+    $doc->input_encoding (undef);
+    ## NOTE: There should be no well-formedness error; if there is,
+    ## then it is an error of the test case itself.
+  } else {
+    $doc = Whatpm::NanoDOM::Document->new;
+    Whatpm::HTML->parse_string ($test->{data} => $doc);
+  }
+
+  my @error;
+  Whatpm::ContentChecker->check_element
+    ($doc->document_element, sub {
+       my %opt = @_;
+       if ($opt{type} =~ /^status:/ and $opt{level} eq 'i') {
+         #
+       } else {
+         push @error, get_node_path ($opt{node}) . ';' . $opt{type} .
+             (defined $opt{text} ? ';' . $opt{text} : '') .
+             (defined $opt{level} ? ';'.$opt{level} : '');
+       }
+     }, sub {
+       my $opt = shift;
+       push @error, get_node_path ($opt->{container_node}) . ';SUBDOC';
+     });
+  
+  ok join ("\n", sort {$a cmp $b} @error),
+    join ("\n", sort {$a cmp $b} @{$test->{errors}}), $test->{data};
+} # test
+
+sub get_node_path ($) {
+  my $node = shift;
+  my @r;
+  while (defined $node) {
+    my $rs;
+    if ($node->node_type == 1) {
+      $rs = $node->manakai_local_name;
+      $node = $node->parent_node;
+    } elsif ($node->node_type == 2) {
+      $rs = '@' . $node->manakai_local_name;
+      $node = $node->owner_element;
+    } elsif ($node->node_type == 3) {
+      $rs = '"' . $node->data . '"';
+      $node = $node->parent_node;
+    } elsif ($node->node_type == 9) {
+      $rs = '';
+      $node = $node->parent_node;
+    } else {
+      $rs = '#' . $node->node_type;
+      $node = $node->parent_node;
+    }
+    unshift @r, $rs;
+  }
+  return join '/', @r;
+} # get_node_path
+
+=head1 NAME
+
+content-checker.pl - Test engine for document conformance checking
+
+=head1 DESCRIPTION
+
+The C<content-checker.pl> script implements a test engine for the
+conformance checking modules, directly or indirectly referenced from
+L<Whatpm::ContentChecker>.
+
+This script is C<require>d by various test scripts, including
+C<ContentCheker.t>, C<ContentChecker-Atom.t>, C<HTML-tokenizer-2.t>,
+and C<LangTag.t>.
+
+=head1 AUTHOR
+
+Wakaba <w@suika.fam.cx>.
+
+=head1 LICENSE
+
+Public Domain.
+
+=cut
+
+1; ## $Date: 2008/09/18 05:31:46 $
