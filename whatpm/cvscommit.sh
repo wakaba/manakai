d64c0b4e777958ec09175c3a41ffc00d1c282b30
Wakaba <w@suika.fam.cx>
2008-10-21 05:03:24 +0000

    ++ ChangeLog	21 Oct 2008 05:03:12 -0000 2008-10-21  Wakaba  <wakaba@suika.fam.cx>
    	* cvscommit.sh: Add a line to invoke |mkcommitfeed.pl|.
    
    	* mkcommitfeed.pl: New script.

--- whatpm/cvscommit.sh
+++ whatpm/cvscommit.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 find -name ChangeLog | xargs cvs diff | grep "^\+" | sed -e "s/^\+//; s/^\+\+ .\//++ whatpm\//" > .cvslog.tmp
 cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
+perl mkcommitfeed.pl --file-name whatpm-commit.en.atom.u8 \
+    --feed-url http://suika.fam.cx/www/markup/html/whatpm/whatpm-commit \
+    --feed-lang en \
+    < .cvslog.tmp
 rm .cvslog.tmp
 
-## $Date: 2007/04/24 13:35:21 $
+## $Date: 2008/10/21 05:03:24 $
 ## License: Public Domain
