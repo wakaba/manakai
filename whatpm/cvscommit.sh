aeb6231106191970645f3f906dca684f82b36755
Wakaba <w@suika.fam.cx>
2008-10-21 05:04:36 +0000

    ++ ChangeLog	21 Oct 2008 05:04:33 -0000 	* cvscommit.sh: Add |-I| option to the |perl| command.
    2008-10-21  Wakaba  <wakaba@suika.fam.cx>

--- whatpm/cvscommit.sh
+++ whatpm/cvscommit.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 find -name ChangeLog | xargs cvs diff | grep "^\+" | sed -e "s/^\+//; s/^\+\+ .\//++ whatpm\//" > .cvslog.tmp
 cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
-perl mkcommitfeed.pl --file-name whatpm-commit.en.atom.u8 \
+## TODO: Don't use -I here
+perl \
+    -I/home/wakaba/work/manakai2/lib/ \
+    mkcommitfeed.pl --file-name whatpm-commit.en.atom.u8 \
     --feed-url http://suika.fam.cx/www/markup/html/whatpm/whatpm-commit \
     --feed-lang en \
     < .cvslog.tmp
 rm .cvslog.tmp
 
-## $Date: 2008/10/21 05:03:24 $
+## $Date: 2008/10/21 05:04:36 $
 ## License: Public Domain
