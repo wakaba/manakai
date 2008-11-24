e8f555f944d9178e1e3e4b29e21f5aa490b7da12
Wakaba <w@suika.fam.cx>
2008-11-24 07:45:31 +0000

    ++ manakai/lib/Message/CGI/ChangeLog	24 Nov 2008 07:43:56 -0000 2008-11-24  Wakaba  <wakaba@suika.fam.cx>
    	* Util.pm: New module (Functions originally developed for
    	SuikaWiki4).

---
+++ lib/Message/CGI/Util.pm
@@ -0,0 +1,50 @@
+package Message::CGI::Util;
+use strict;
+
+use Exporter;
+push our @ISA, 'Exporter';
+
+our @EXPORT_OK = qw/
+  htescape
+  percent_encode percent_encode_na
+  percent_decode
+  get_absolute_url
+/;
+
+require Encode;
+
+sub htescape ($) {
+  my $s = shift;
+  $s =~ s/&/&amp;/g;
+  $s =~ s/</&lt;/g;
+  $s =~ s/"/&quot;/g;
+  return $s;
+} # htescape
+
+sub percent_encode ($) {
+  my $s = Encode::encode ('utf8', $_[0]);
+  $s =~ s/([^A-Za-z0-9_~-])/sprintf '%%%02X', ord $1/ges;
+  return $s;
+} # percent_encode
+
+sub percent_encode_na ($) {
+  my $s = Encode::encode ('utf8', $_[0]);
+  $s =~ s/([^\x00-\x7F])/sprintf '%%%02X', ord $1/ges;
+  return $s;
+} # percent_encode_na
+
+sub percent_decode ($) { # input should be a byte string.
+  my $s = shift;
+  $s =~ s/%([0-9A-Fa-f]{2})/pack 'C', hex $1/ge;
+  return Encode::decode ('utf-8', $s); # non-UTF-8 octet converted to \xHH
+} # percent_decode
+
+sub get_absolute_url ($$) {
+  require Message::DOM::DOMImplementation;
+  return Message::DOM::DOMImplementation->create_uri_reference ($_[0])
+      ->get_absolute_reference ($_[1])
+      ->get_uri_reference 
+      ->uri_reference;
+} # get_absolute_url
+
+1;
