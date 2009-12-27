3fc4e2a7ea5c8f13d3d9925e6ab9edbb79ee0a00
Wakaba <w@suika.fam.cx>
2009-12-27 18:26:53 +0900

    implemented script element content validation (HTML5 revision 4178, HTML5 revision 4179)

--- lib/Whatpm/HTML/ScriptChecker.pm
+++ lib/Whatpm/HTML/ScriptChecker.pm
@@ -4,12 +4,86 @@ use warnings;
 our $VERSION = '1.0';
 
 use constant STATE_INITIAL => 0;
+
 use constant STATE_COMMENT => 1;
 use constant STATE_AFTER_LINE_COMMENT => 2;
+
+use constant STATE_AFTER_CDO => 3;
+use constant STATE_AFTER_SCRIPT_START => 4;
 
 my $level = {
     must => 'm',
 };
+
+sub check_element_text ($$$) {
+  my $class = shift;
+  my $sref = ref $_[0] ? $_[0] : \( $_[0] );
+  my $onerror = $_[1];
+
+  pos $$sref = 0;
+  my ($l, $c) = (1, 1);
+
+  my $prev_pos = 0;
+  my $set_lc = sub ($) {
+    my $v = substr $$sref, $prev_pos, $+[0] - $prev_pos + ($_[0] || 0);
+    $prev_pos = $+[0];
+    ($l++, $c = 1) while $v =~ s/^[^\x0D\x0A]*(\x0A|\x0D\x0A?)//;
+    $c += length $v;
+  }; # $set_lc
+
+  my $state = STATE_INITIAL;
+  
+  while (pos $$sref < length $$sref) {
+    if ($state == STATE_INITIAL) {
+      if ($$sref =~ /<!--/gc) {
+        $set_lc->();
+        $state = STATE_AFTER_CDO;
+      } else {
+        return;
+      }
+    } elsif ($state == STATE_AFTER_CDO) {
+      if ($$sref =~ m[(-->|<[Ss][Cc][Rr][Ii][Pp][Tt][>\x09\x0A\x0C\x20/])]gc) {
+        if ($1 eq '-->') {
+          $set_lc->();
+          $state = STATE_INITIAL;
+        } else {
+          $set_lc->();
+          $state = STATE_AFTER_SCRIPT_START;
+        }
+      } else {
+        //;
+        $set_lc->(1);
+        last;
+      }
+    } elsif ($state == STATE_AFTER_SCRIPT_START) {
+      if ($$sref =~ m[(-->|</[Ss][Cc][Rr][Ii][Pp][Tt][>\x09\x0A\x0C\x20/])]gc) {
+        if ($1 eq '-->') {
+          $set_lc->(-3);
+          last;
+        } else {
+          $set_lc->();
+          $state = STATE_AFTER_CDO;
+        }
+      } else {
+        //;
+        $set_lc->(1);
+        last;
+      }
+    } else {
+      die "Unknown state: $state";
+    }
+  }
+
+  if ($state == STATE_AFTER_CDO) {
+    $onerror->(type => 'script:content:no cdc', # XXX documentation
+               level => $level->{must},
+               line => $l, column => $c);
+  } elsif ($state == STATE_AFTER_SCRIPT_START) {
+    $onerror->(type => 'script:content:no /script', # XXX documentation
+               level => $level->{must},
+               line => $l, column => $c);
+  }
+} # check_element_content
 
 sub check_inline_documentation ($$$) {
   my $class = shift;
@@ -18,13 +92,6 @@ sub check_inline_documentation ($$$) {
 
   pos $$sref = 0;
   my ($l, $c) = (1, 1);
-
-  my $set_lc = sub {
-    my $v = $_[0];
-    ($l++, $c = 1) while $v =~ s/^[^\x0D\x0A]*(\x0A|\x0D\x0A?)//;
-    $c += length $v;
-  }; # $set_lc
-
   my $state = STATE_INITIAL;
   
   while (pos $$sref < length $$sref) {
--- t/Whatpm-HTML-ScriptChecker.t
+++ t/Whatpm-HTML-ScriptChecker.t
@@ -7,12 +7,31 @@ use lib file (__FILE__)->dir->stringify;
 use Whatpm::HTML::ScriptChecker;
 use Test;
 
-BEGIN { plan tests => 16 }
+BEGIN { plan tests => 47 }
 
 BEGIN { require 'testfiles.pl' };
 
-my $test_input_f = file (__FILE__)->dir->subdir ('html-misc')->file ('script-inline-docs.dat');
-execute_test $test_input_f, {
+my $text_input_f = file (__FILE__)->dir->subdir ('html-misc')->file ('script-text.dat');
+execute_test $text_input_f, {
+  data => {is_prefixed => 1},
+  errors => {is_list => 1},
+}, sub ($) {
+  my $test = shift;
+  my $data = $test->{data}->[0];
+
+  my @error;
+  my $onerror = sub ($) {
+    my %args = @_;
+    push @error, join ';', map { defined $_ ? $_ : '' } @args{qw(line column level type value)};
+  }; # $onerror
+
+  Whatpm::HTML::ScriptChecker->check_element_text (\$data, $onerror);
+
+  ok join ("\n", @error), join ("\n", @{$test->{errors}->[0] or []});
+};
+
+my $inline_input_f = file (__FILE__)->dir->subdir ('html-misc')->file ('script-inline-docs.dat');
+execute_test $inline_input_f, {
   data => {is_prefixed => 1},
   errors => {is_list => 1},
 }, sub ($) {
