dc02748f901c99355dcd2a3d74e0aecb201092e3
Wakaba <w@suika.fam.cx>
2010-07-25 15:52:30 +0900

    for testing

---
+++ sketch/validate-xml.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Path::Class;
+use lib file (__FILE__)->dir->parent->subdir ('lib')->stringify;
+use lib glob file (__FILE__)->dir->parent->subdir ('modules/*/lib');
+
+my $input = shift;
+
+use Message::DOM::DOMImplementation;
+my $dom = Message::DOM::DOMImplementation->new;
+my $doc = $dom->create_document;
+
+$doc->inner_html ($input);
+
+use Whatpm::ContentChecker;
+Whatpm::ContentChecker->check_document ($doc, sub {
+  my %err = @_;
+  my $line = delete $err{line} || 0;
+  my $column = delete $err{column} || 0;
+  my $level = delete $err{level} || '?';
+  my $type = delete $err{type} || '';
+  if ($err{node}) {
+    $err{node} = $err{node}->local_name || $err{node}->node_name;
+  }
+  printf STDOUT "%s[%d.%d]: %s, %s\n",
+      $level, $line, $column, $type,
+      join ' ', %err;
+});
