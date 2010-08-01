672c978d28beedf86a85e50a5171d653890a5d45
Wakaba <w@suika.fam.cx>
2010-08-01 23:25:01 +0900

    discard unclosed markup declarations, II

--- lib/Whatpm/HTML/Tokenizer.pm
+++ lib/Whatpm/HTML/Tokenizer.pm
@@ -6191,11 +6191,10 @@ sub _get_next_token ($) {
   
         return  ($self->{ct}); # ATTLIST
         redo A;
-      } elsif ($nc == -1) {
+      } elsif ($nc == EOF_CHAR) {
         ## XML5: No parse error.
         $self->{parse_error}->(level => $self->{level}->{must}, type => 'unclosed md'); ## TODO: type
         $self->{state} = DOCTYPE_INTERNAL_SUBSET_STATE; ## XML5: "Data state".
-        return  ($self->{ct});
         redo A;
       } else {
         ## XML5: Not defined yet.
