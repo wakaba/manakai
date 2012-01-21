5a89b79fb708b762f26ba2650aebdccc005d9150
Wakaba <w@suika.fam.cx>
2012-01-21 14:04:59 +0900

    Obsolete

--- demo/css-parser-cgi/cvscommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-find -name ChangeLog | xargs cvs diff | grep "^\+" | sed -e "s/^\+//; s/^\+\+ .\//++ css\//" > .cvslog.tmp
-cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
-rm .cvslog.tmp
-
-## $Date: 2007/09/08 01:30:20 $
-## License: Public Domain
