ed5d561c62ccbcea824ba111b81bbb4ffb97e140
Wakaba <w@suika.fam.cx>
2014-03-01 15:31:48 +0900

    make dataautoupdate

--- lib/Whatpm/_LangTagReg_Full.pm
+++ lib/Whatpm/_LangTagReg_Full.pm
@@ -59480,11 +59480,6 @@ $Whatpm::LangTag::RegistryFull = {
                                                       'Luanda, Angola'
                                                     ]
                                  },
-                      'aqams' => {
-                                   'Description' => [
-                                                      'Amundsen-Scott Station, South Pole'
-                                                    ]
-                                 },
                       'aqcas' => {
                                    'Description' => [
                                                       'Casey Station, Bailey Peninsula'
@@ -59883,11 +59878,6 @@ $Whatpm::LangTag::RegistryFull = {
                       'camon' => {
                                    'Description' => [
                                                       'Moncton, Canada'
-                                                    ]
-                                 },
-                      'camtr' => {
-                                   'Description' => [
-                                                      'Montreal, Canada'
                                                     ]
                                  },
                       'canpg' => {
@@ -61400,11 +61390,6 @@ $Whatpm::LangTag::RegistryFull = {
                                                       'Metlakatla (Alaska), United States'
                                                     ]
                                  },
-                      'usnavajo' => {
-                                      'Description' => [
-                                                         'Shiprock (Navajo), United States'
-                                                       ]
-                                    },
                       'usndcnt' => {
                                      'Description' => [
                                                         'Center (North Dakota), United States'
